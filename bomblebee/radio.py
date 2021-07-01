439329512b02280967c0f58b6492f3a6e780592b
Rafael Corrêa <rafaelsouzac@gmail.com>
2021-07-01 15:36:58 -0300

    Quarto commit

--- bomblebee/radio.py
+++ bomblebee/radio.py
@@ -1,2 +1,7 @@
 # arquivo de verificação dos atendentes
 # é o encarregado de verificar qual robo é especializado para o atendimento.
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='../dirigindo/chromedriver.exe')
+driver.get('https://www.facebook.com')
